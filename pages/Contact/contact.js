3c6f72831bf7fd198fa362580bfd2966d39dc8c1
Preet2099x <preet2099x@gmail.com>
2024-06-07 06:23:00 +0530

    overhaul

--- pages/Contact/contact.js
+++ pages/Contact/contact.js
@@ -1,5 +1,4 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    
+document.addEventListener('DOMContentLoaded', () => {
     const emailInput = document.querySelector('input[type="email"]');
     const submitButton = document.querySelector('.submit button');
     const contactForm = document.getElementById('contactForm');
@@ -10,13 +9,11 @@ document.addEventListener('DOMContentLoaded', (event) => {
         if (validateEmail(email)) {
             alert('Thank you for subscribing!');
             console.log(emailInput);
-            // Here you can perform any additional actions you want after successful submission
         } else {
             alert('Please enter a valid email address.');
         }
     });
 
-    // Add event listener for form submission
     contactForm.addEventListener('submit', (event) => {
         event.preventDefault();
         const formData = new FormData(contactForm);
@@ -25,9 +22,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
             formDataObject[key] = value;
         });
         console.log(formDataObject);
-        // Display an alert when the form is submitted
         alert('Form submitted successfully!');
-        // Here you can handle the form submission, for example, send the data to a server using fetch() or XMLHttpRequest
     });
 
     function validateEmail(email) {
